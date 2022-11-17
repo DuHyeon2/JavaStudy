b5254859f9fe1330adb73be52226030e1b6431d1
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-17 17:56:37 +0900

    update

--- Baekjoon/개수세기_10807번/Main.java
+++ Baekjoon/개수세기_10807번/Main.java
@@ -15,6 +15,5 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Solution(sc.nextInt(), sc.));
     }
 }
